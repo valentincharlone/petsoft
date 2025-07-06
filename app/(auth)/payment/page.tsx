3e115d88c87ce27ad807b749aeb1f9b3c90932bc
valentincharlone <valencharlone@gmail.com>
2025-07-06 20:55:14 -0300

    feat: add payment page component; create layout for payment access with header

---
+++ app/(auth)/payment/page.tsx
@@ -0,0 +1,9 @@
+import H1 from "@/components/h1";
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center space-y-10">
+      <H1>Petsoft access required payment</H1>
+    </main>
+  );
+}
