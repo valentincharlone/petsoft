ff64d659f1d826fd619622433cdde6ac7e3930b9
valentincharlone <valencharlone@gmail.com>
2025-07-07 12:47:18 -0300

    feat: update Stripe webhook handling; switch to text request body and handle checkout session completion to update user access

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,15 +1,34 @@
 import prisma from "@/lib/db";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  const body = await request.text();
+  const signature = request.headers.get("stripe-signature")!;
 
-  await prisma?.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  });
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error) {
+    return Response.json(null, { status: 400 });
+  }
+  switch (event.type) {
+    case "checkout.session.completed":
+      await prisma?.user.update({
+        where: {
+          email: event.data.object.customer_email,
+        },
+        data: {
+          hasAccess: true,
+        },
+      });
+      break;
+    default:
+      console.log("Unhandled event type", event.type);
+  }
+
   return Response.json(null, { status: 200 });
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -50,11 +50,19 @@ const config = {
       if (isLoggedIn && isTryingToAccessApp && auth?.user.hasAccess) {
         return true;
       }
+      if (
+        isLoggedIn &&
+        (request.nextUrl.pathname.includes("/login") ||
+          request.nextUrl.pathname.includes("/signup")) &&
+        auth?.user.hasAccess
+      ) {
+        return Response.redirect(new URL("/app/dashboard", request.nextUrl));
+      }
+
       if (isLoggedIn && !isTryingToAccessApp) {
         if (
           request.nextUrl.pathname.includes("/login") ||
-          (request.nextUrl.pathname.includes("/signup") &&
-            !auth?.user.hasAccess)
+          request.nextUrl.pathname.includes("/signup")
         ) {
           return Response.redirect(new URL("/payment", request.nextUrl));
         }
